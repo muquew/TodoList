7cda5f25a752eaf101724438f0c97772277a1ab5
muquew <106162971+muquew@users.noreply.github.com>
2023-11-28 00:40:44 +0800

    Delete TodoList4 directory

--- TodoList4/src-tauri/src/setup.rs
+++
@@ -1,8 +0,0 @@
-use tauri::{App, Manager};
-use crate::get_lang;
-
-#[cfg(windows)]
-pub(crate) fn setup(app: &App) {
-    let window = &app.get_window("main").unwrap();
-    let _ = window.set_title(&get_lang::get_json_value("windowsTitle.main"));
-}
--- TodoList4/src-tauri/src/shortcut.rs
+++
@@ -1,7 +0,0 @@
-use tauri::AppHandle;
-use crate::system_tray;
-
-#[tauri::command]
-pub(crate) async fn open_change_window(app_handle: AppHandle) {
-    system_tray::create_change_window(&app_handle);
-}
